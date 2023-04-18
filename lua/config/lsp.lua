4814a1edc9c32da617b27c3b346fa12e6373bfa2
Miloš Hadžić <milos@rightfold.io>
2023-04-18 15:46:03 +0200

    Sync some dotfile updates

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,65 @@
 -- nvim_lsp object
 local nvim_lsp = require'lspconfig'
 
+local cmp = require'cmp'
+
+cmp.setup({
+    preselect = cmp.PreselectMode.None,
+    view = {
+      entries = "native"
+    },
+    mapping = {
+      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.close(),
+      ['<CR>'] = cmp.mapping.confirm({
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true
+      }),
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = "hrsh7th/cmp-nvim-lua" },
+      { name = "hrsh7th/cmp-nvim-lsp-signature-help" },
+      { name = 'path' },
+    }, {
+      { name = "buffer", keyword_length = 3 },
+    })
+})
+
+-- Enable diagnostics
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = true,
+    signs = true,
+    update_in_insert = false,
+  }
+)
+
+
+-- Setup buffer-local keymaps / options for LSP buffers
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lsp_attach = function(client, buf)
+	-- Example maps, set your own with vim.api.nvim_buf_set_keymap(buf, "n", <lhs>, <rhs>, { desc = <desc> })
+	-- or a plugin like which-key.nvim
+	-- <lhs>        <rhs>                        <desc>
+	-- "K"          vim.lsp.buf.hover            "Hover Info"
+	-- "<leader>qf" vim.diagnostic.setqflist     "Quickfix Diagnostics"
+	-- "[d"         vim.diagnostic.goto_prev     "Previous Diagnostic"
+	-- "]d"         vim.diagnostic.goto_next     "Next Diagnostic"
+	-- "<leader>e"  vim.diagnostic.open_float    "Explain Diagnostic"
+	-- "<leader>ca" vim.lsp.buf.code_action      "Code Action"
+	-- "<leader>cr" vim.lsp.buf.rename           "Rename Symbol"
+	-- "<leader>fs" vim.lsp.buf.document_symbol  "Document Symbols"
+	-- "<leader>fS" vim.lsp.buf.workspace_symbol "Workspace Symbols"
+	-- "<leader>gq" vim.lsp.buf.formatting_sync  "Format File"
+
+	vim.api.nvim_buf_set_option(buf, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+	vim.api.nvim_buf_set_option(buf, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_buf_set_option(buf, "tagfunc", "v:lua.vim.lsp.tagfunc")
+end
+
 -- Configure LSP through rust-tools.nvim plugin.
 -- rust-tools will configure and enable certain LSP features for us.
 -- See https://github.com/simrat39/rust-tools.nvim#configuration
@@ -22,7 +81,8 @@ local opts = {
   -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
   server = {
     -- on_attach is a callback called when the language server attachs to the buffer
-    on_attach = on_attach,
+    on_attach = lsp_attach,
+    capabilities = capabilities,
     settings = {
       -- to enable rust-analyzer settings visit:
       -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
@@ -37,42 +97,6 @@ local opts = {
 }
 
 require("rust-tools").setup(opts)
-
-local cmp = require'cmp'
-
-cmp.setup({
-    preselect = cmp.PreselectMode.None,
-    snippet = {
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-      end,
-    },
-    mapping = {
-      ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.close(),
-      ['<CR>'] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true
-      }),
-    },
-    sources = {
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' },
-      { name = 'buffer' },
-      { name = 'path' },
-    }
-})
-
--- Enable diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = false,
-  }
-)
 
 require'lspconfig'.tsserver.setup{}
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,6 +33,8 @@ return require('packer').startup(function()
   use("hrsh7th/nvim-cmp")
   use({
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lua",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-vsnip",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
